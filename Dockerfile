a43f49792917db6cebffa3f1ce3fceb3a431f181
Asbjørn Enge <asbjorn@hanafjedle.net>
2016-09-15 11:13:21 +0200

    Fixed container build

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
 FROM asbjornenge/node-zero:4.x-1.0.0
 ADD . /app
-WORKDIR /APP
-CMD ["start.sh"]
+CMD ["/app/start.sh"]
--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 init-zerotier || exit 1
 export ZT_NC_NETWORK=/var/lib/zerotier-one/nc_$ZEROTIER_NETWORK_ID
 export LD_PRELOAD=/libztintercept.so
-node index.js
+node /app/index.js
